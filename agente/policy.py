5332bd623d8ddfe1bfa2805adf10b68deaf393d7
Alfonso! <30917026+alfonsoopazom@users.noreply.github.com>
2021-04-23 01:54:00 -0400

    Env: Configuracion del entorno virtual de Anaconda para el proyecto

---
+++ agente/policy.py
@@ -0,0 +1,8 @@
+
+def policyPrint():
+    print("Mi funcion sobre politica")
+
+def plus(a, b):
+    c = a + b
+    print(c)
+
--- init.py
+++ init.py
@@ -1,1 +1,17 @@
-print("Agente de aprendizaje por refuerzo - FER")
+import agente.policy as politica
+import numpy as npm
+from agente.policy import policyPrint, plus
+
+
+def printNumpy():
+    a = npm.arange(15).reshape(3, 5)
+    print(a.shape)
+
+def main():
+    politica.policyPrint()
+    policyPrint()
+    plus(1,2)
+    printNumpy()
+
+if __name__ == '__main__':
+    main()
