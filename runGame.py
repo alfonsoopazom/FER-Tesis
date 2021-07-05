15414bcf89c44cc047e7057ca88c1ecc5d626acc
alfonso opazo <alfonso.opazo@bennu.cl>
2021-07-05 11:41:05 -0400

    Feat/Se grafican valores Q, epsilon y rewards

--- runGame.py
+++ runGame.py
@@ -12,6 +12,7 @@ np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 Qvalues = np.zeros([1, 200])
 rew = []
 epsilon = []
+qvaluesAux = []
 gamma = 0.9
 
 q_values_file = open("qValues.txt", "w")
@@ -19,7 +20,7 @@ q_values_file = open("qValues.txt", "w")
 """Game parameters"""
 gameType = ChefsHatEnv.GAMETYPE["MATCHES"]
 
-gameStopCriteria = 10
+gameStopCriteria = 5
 rewardFunction = RewardOnlyWinning.RewardOnlyWinning()
 
 """Player Parameters"""
@@ -61,20 +62,20 @@ for a in range(episodes):
         info = {"validAction": False}
 
         while not info["validAction"]:
+
             nextobs, reward, isMatchOver, info = env.step(action)
 
-            agent1.actionUpdate(observations, nextobs, action, reward, info)
-            epsilon.append(agent1.epsilon)
-            rew.append(reward)
-            Qvalues = agent1.matchUpdate(info)
-
-            #print(len(Qvalues))
-            # print(epsilon)
-            # agente = agent1.buildSimpleModel().summary()
+        agent1.actionUpdate(observations, nextobs, action, reward, info)
+        rew.append(reward)
 
         if isMatchOver:
-            '''for p in playersAgents:
-                p.matchUpdate(info)'''
+
+            for p in playersAgents:
+                q = agent1.matchUpdate(info)
+                b = np.max(q)
+                qvaluesAux.append(b)
+                epsilon.append(agent1.epsilon)
+
 
             print("-------------")
             print("Match:" + str(info["matches"]))
@@ -84,30 +85,29 @@ for a in range(episodes):
 
 #agent1.pSuccess(Qvalues, rew)
 
-for row in Qvalues:
+for row in qvaluesAux:
     np.savetxt(q_values_file, row)
 q_values_file.close()
 
-plt.plot(Qvalues)
-plt.title("Q-Values de Introspection")
-plt.xlabel('Episodios')
+plt.plot(qvaluesAux, Label='Q-values')
+plt.title("Q-Values Introspection entrenamiento")
+plt.xlabel('Steps')
 plt.ylabel('Q-Values')
-plt.xlim([0, 10])
 plt.legend()
 plt.savefig("q_values.png")
 plt.close()
 
 plt.plot(epsilon, label="Valores de Epsilon")
-plt.title("Epsilon de Introspection")
-plt.xlabel('Episodios')
+plt.title("Epsilon de Introspection entrenamiento")
+plt.xlabel('Steps')
 plt.ylabel('Epsilon')
 plt.legend()
 plt.savefig("valores_epsilon.png")
 plt.close()
 
 plt.plot(rew, label="Recompensas ")
-plt.title("Recompensas de Introspection")
-plt.xlabel('Episodios')
+plt.title("Recompensas de Introspection entrenamiento")
+plt.xlabel('Steps')
 plt.ylabel('Rewards')
 plt.legend()
 plt.ylim([-0.5, 2])
@@ -119,7 +119,7 @@ env.startExperiment(rewardFunctions=rewards, gameType=gameType, stopCriteria=gam
                     logDirectory=saveDirectory, verbose=verbose, saveDataset=True, saveLog=True)
 
 """Start Environment"""
-for a in range(episodes):
+'''for a in range(episodes):
 
     observations = env.reset()
 
@@ -139,4 +139,4 @@ for a in range(episodes):
             print("Match:" + str(info["matches"]))
             print("Score:" + str(info["score"]))
             print("Performance:" + str(info["performanceScore"]))
-            print("-------------")
+            print("-------------")'''
